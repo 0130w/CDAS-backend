6c637906f7ee1d77a6aa2282086e9ec8b9adcccd
0130w <1157374677@qq.com>
2023-07-16 15:27:03 +0800

    modify logincontroller

--- src/main/java/mingroup/AnalysisSys/controller/user/LoginController.java
+++ src/main/java/mingroup/AnalysisSys/controller/user/LoginController.java
@@ -5,6 +5,7 @@ import mingroup.AnalysisSys.entity.User;
 import mingroup.AnalysisSys.mapper.CollegeMapper;
 import mingroup.AnalysisSys.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class LoginController {
 
     @Autowired
