c867e66e8ad20dc1768ad0784d02fc69669b2e85
0130w <1157374677@qq.com>
2023-07-18 01:39:50 +0800

    change register module position

--- src/main/java/mingroup/AnalysisSys/controller/user/RegisterController.java
+++ src/main/java/mingroup/AnalysisSys/controller/user/RegisterController.java
@@ -1,4 +1,22 @@
 package mingroup.AnalysisSys.controller.user;
 
+import mingroup.AnalysisSys.entity.User;
+import mingroup.AnalysisSys.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
 public class RegisterController {
+    @Autowired
+    UserMapper userMapper;
+
+    @PostMapping("/register")
+    public boolean register(@RequestBody User user) {
+        // if grade property is not specified, the default value equals to 0
+        return userMapper.insertUser(user);
+    }
 }
